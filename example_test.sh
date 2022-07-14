ef85ab0f296ab4c861bf68a8b5992930e038f730
Andrej Podhradsky <apodhrad@redhat.com>
2022-07-14 12:03:02 +0200

    Update example_test with bunit-0.0.3
    Signed-off-by: Andrej Podhradsky <apodhrad@redhat.com>

--- example_test.sh
+++ example_test.sh
@@ -1,21 +1,21 @@
 #/bin/bash
 
-source <(curl -s "https://raw.githubusercontent.com/qetools/bunit/0.0.2/bunit.sh")
+source <(curl -s "https://raw.githubusercontent.com/apodhrad/bunit/0.0.3/bunit.sh")
 source "./example.sh"
 
 scenario "test saying hello with a method"
 result=$(say_hello "Test1")
-assertEquals "$result" "Hello Test1"
+assert_equals "Hello Test1" "${result}" 
 print_result
 
 scenario "test saying hello with executing the script"
 result=$(./example.sh "Test2")
-assertEquals "$result" "Hello Test2"
+assertEquals "Hello Test2" "${result}" 
 print_result
 
 scenario "test saying hello with sourcing the script"
 result=$(source "./example.sh")
-[[ -n "$result" ]] && fail "The sourced script produced an output! Output: '$result'"
+[[ -n "${result}" ]] && fail "The sourced script produced an output! Output: '${result}'"
 print_result
 
 print_final_result
